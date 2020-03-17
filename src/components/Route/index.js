a30aeaef9f29cccfaac6982c774032ad1b3c2344
Denver Chen <ycctb2000@163.com>
2020-03-17 11:34:33 +0800

    升级react-router-dom， 新版本提供hooks. 更容易的获取history等信息.

--- src/components/Route/index.js
+++ src/components/Route/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Route, Link, Redirect, BrowserRouter } from 'react-router-dom';
+import { Route, Link, Redirect, HashRouter, NavLink } from 'react-router-dom';
 import loadable from '@loadable/component';
 
 export default props => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div>
         <Link to="/home">Home</Link>|<Link to="/about">About</Link>
       </div>
 
       <Route exact path="/home" component={loadable(() => import('../Home'))} />
-      <Route exact path="/about" component={loadable(() => import('../About'))} />
+      <Route exact={false} path="/about" component={loadable(() => import('../About'))} />
       <Redirect to='/home' />
-    </BrowserRouter>
+    </HashRouter>
   );
 };
